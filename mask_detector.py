084df1e12cbc289376bc24f2437c7591eddf34df
Mohammed Fasil V <fasilmveloor@gmail.com>
2023-09-20 20:43:32 +0530

    add gui for test code using pyqt6

---
+++ mask_detector.py
@@ -0,0 +1,96 @@
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+import numpy as np
+import imutils
+import time
+import cv2
+import os
+import sys
+from PyQt6 import QtCore, QtGui, QtWidgets
+from PIL import ImageQt
+
+class MaskDetector(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Mask Detector")
+
+        self.label = QtWidgets.QLabel(self)
+        self.setCentralWidget(self.label)
+
+        self.faceNet = cv2.dnn.readNet("face_detector/deploy.prototxt", "face_detector/res10_300x300_ssd_iter_140000.caffemodel")
+        self.maskNet = load_model("mask_detector.model")
+
+        self.vs = VideoStream(src=0).start()
+
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.update_frame)
+        self.timer.start(10)
+
+    def update_frame(self):
+        frame = self.vs.read()
+        frame = imutils.resize(frame, width=400)
+
+        (locs, preds) = self.detect_and_predict_mask(frame, self.faceNet, self.maskNet)
+
+        for (box, pred) in zip(locs, preds):
+            (startX, startY, endX, endY) = box
+            (mask, withoutMask) = pred
+
+            label = "Mask" if mask > withoutMask else "No Mask"
+            color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+            label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+            cv2.putText(frame, label, (startX, startY - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+            cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+        img = QtGui.QImage(frame.data, frame.shape[1], frame.shape[0], QtGui.QImage.Format(QtGui.QImage.Format.Format_RGB888)).rgbSwapped()
+        pixmap = QtGui.QPixmap.fromImage(img)
+        self.label.setPixmap(pixmap)
+
+    def detect_and_predict_mask(self, frame, faceNet, maskNet):
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224), (104.0, 177.0, 123.0))
+        faceNet.setInput(blob)
+        detections = faceNet.forward()
+
+        faces = []
+        locs = []
+        preds = []
+
+        for i in range(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > 0.5:
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+
+                (startX, startY) = (max(0, startX), max(0, startY))
+                (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+                face = frame[startY:endY, startX:endX]
+                face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+                face = cv2.resize(face, (224, 224))
+                face = img_to_array(face)
+                face = preprocess_input(face)
+
+                faces.append(face)
+                locs.append((startX, startY, endX, endY))
+
+        if len(faces) > 0:
+            faces = np.array(faces, dtype="float32")
+            preds = maskNet.predict(faces, batch_size=32)
+        
+        return (locs, preds)
+
+    def closeEvent(self, event):
+        self.vs.stop()
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MaskDetector()
+    window.show()
+    sys.exit(app.exec())
